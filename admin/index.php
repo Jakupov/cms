f4ca23ba217f032c6903e365cb73e0edb60bca85
nurlandoskara <nurlandoskara@gmail.com>
2017-02-09 17:14:28 +0500

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -30,7 +30,33 @@
 	<script src="<?php echo SITE ?>admin/components/tinymce/tinymce.min.js"></script>
   	<script>tinymce.init({ 
   		selector:'textarea',
-  		plugins: "code link"
+  		plugins: "code link",
+  		theme: "modern",
+    paste_data_images: true,
+    plugins: [
+      "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+      "searchreplace wordcount visualblocks visualchars code fullscreen",
+      "insertdatetime media nonbreaking save table contextmenu directionality",
+      "emoticons template paste textcolor colorpicker textpattern"
+    ],
+    toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+    toolbar2: "print preview media | forecolor backcolor emoticons",
+    image_advtab: true,
+    file_picker_callback: function(callback, value, meta) {
+		      if (meta.filetype == 'image') {
+		        $('#upload').trigger('click');
+		        $('#upload').on('change', function() {
+		          var file = this.files[0];
+		          var reader = new FileReader();
+		          reader.onload = function(e) {
+		            callback(e.target.result, {
+		              alt: ''
+		            });
+		          };
+		          reader.readAsDataURL(file);
+		        });
+		      }
+		    }
   		});
   	</script>
   	<script src="<?php echo SITE ?>admin/js/func.js?k=<?php echo rand(); ?>"></script>
